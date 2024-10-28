3ce0e24b35f7067ca5a9a2855dcfeb97701c65bd
Chris Gorman <chrisjohgorman@gmail.com>
2024-10-28 06:42:09 -0400

    Initial import of instruction decoder
    - bench will likely be broken as it has not been converted to amaranth
      0.6

---
+++ 04_instruction_decoder/blink.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+""" This module blinks the leds on a Cmod A7 FPGA slowly enough to see """
+
+from soc import SOC
+from clockworks import Clockworks, CLOCKWORKS_DOMAIN_NAME
+from amaranth_boards.cmod_a7 import CmodA7_35Platform
+from amaranth import Module, ClockDomain
+
+
+# A platform contains board specific information about FPGA pin assignments,
+# toolchain and specific information for uploading the bitfile.
+platform = CmodA7_35Platform(toolchain="Xray")
+
+# We need a top level module
+m = Module()
+
+# This is the instance of our SOC
+soc = SOC()
+
+# The SOC is turned into a submodule (fragment) of our top level module.
+m.submodules.soc = soc
+
+# for amaranth version 0.6 define slow domain in top module
+# Instantiate the clockwork with a divider of 2^21
+m.domains += ClockDomain(CLOCKWORKS_DOMAIN_NAME)
+slclk = Clockworks(slow=21)
+
+# Turn the clockwork into a submodule of the top level module
+m.submodules.slclk = slclk
+
+# The platform allows access to the various resources defined by the board
+# definition from amaranth-boards.
+led0 = platform.request('led', 0)
+led1 = platform.request('led', 1)
+rgb = platform.request('rgb_led')
+
+# We connect the SOC leds signal to the various LEDs on the board.
+m.d.comb += [
+    led0.o.eq(soc.leds[0]),
+    led1.o.eq(soc.leds[1]),
+    rgb.r.o.eq(soc.leds[2]),
+    rgb.g.o.eq(soc.leds[3]),
+    rgb.b.o.eq(soc.leds[4]),
+]
+
+# To generate the bitstream, we build() the platform using our top level
+# module m.
+platform.build(m, do_program=True)
---
+++ 04_instruction_decoder/soc.py
@@ -0,0 +1,131 @@
+
+""" This module provides the system on chip (SOC) implementation
+    for the instruction decoder """
+
+from amaranth import Module, Signal, Array, Mux
+from amaranth.lib import wiring
+from amaranth.lib.wiring import Out
+
+
+class SOC(wiring.Component):
+
+    """ This class describes the SOC instruction decoder """
+
+    leds: Out(5)
+
+    def __init__(self):
+
+        super().__init__()
+
+    def elaborate(self, platform):
+
+        """ The clockwork provides a new clock domain called 'slow'.
+            We replace the default sync domain with the new one to have the
+            counter run slower unless we are simulating. """
+
+        m = Module()
+
+        sequence = [
+                #       24      16       8       0
+                # .......|.......|.......|.......|
+                #R         rs2  rs1 f3   rd     op
+                #I         imm  rs1 f3   rd     op
+                #S    imm  rs2  rs1 f3  imm     op
+                # ......|....|....|..|....|......|
+                0b00000000000000000000000000110011, # R add  x0, x0, x0
+                0b00000000000000000000000010110011, # R add  x1, x0, x0
+                0b00000000000100001000000010010011, # I addi x1, x1,  1
+                0b00000000000100001000000010010011, # I addi x1, x1,  1
+                0b00000000000100001000000010010011, # I addi x1, x1,  1
+                0b00000000000100001000000010010011, # I addi x1, x1,  1
+                0b00000000000000001010000100000011, # I lw   x2, 0(x1)
+                0b00000000000100010010000000100011, # S sw   x2, 0(x1)
+                0b00000000000100000000000001110011  # S ebreak
+        ]
+
+        # Signal declarations
+        isALUreg = Signal()
+        isALUimm = Signal()
+        isBranch = Signal()
+        isJALR = Signal()
+        isJAL = Signal()
+        isAUIPC = Signal()
+        isLUI = Signal()
+        isLoad = Signal()
+        isStore = Signal()
+        isSystem = Signal()
+
+        Uimm, Iimm, Bimm, Simm, Jimm = [Signal(32) for _ in range(5)]
+        rs1Id, rs2Id, rdId = [Signal(5) for _ in range(3)]
+        funct3, funct7 = [Signal(3), Signal(7)]
+
+        pc = Signal(32)
+        instr = Signal(32, reset=0b0110011)
+        mem = Array([Signal(32, reset=x) for x in sequence])
+
+        self.pc = pc
+        self.instr = instr
+        self.isALUreg = isALUreg
+        self.isALUimm = isALUimm
+        self.isSystem = isSystem
+        self.rdId = rdId
+        self.rs1Id = rs1Id
+        self.rs2Id = rs2Id
+        self.Iimm = Iimm
+        self.funct3 = funct3
+
+        # Opcode decoder
+        m.d.comb += [
+            isALUreg.eq(instr[0:7] == 0b0110011),
+            isALUimm.eq(instr[0:7] == 0b0010011),
+            isBranch.eq(instr[0:7] == 0b1100011),
+            isJALR.eq(  instr[0:7] == 0b1100111),
+            isJAL.eq(   instr[0:7] == 0b1101111),
+            isAUIPC.eq( instr[0:7] == 0b0010111),
+            isLUI.eq(   instr[0:7] == 0b0110111),
+            isLoad.eq(  instr[0:7] == 0b0000011),
+            isStore.eq( instr[0:7] == 0b0100011),
+            isSystem.eq(instr[0:7] == 0b1110011),
+        ]
+
+        # Immediate format decoder
+        m.d.comb += [
+            Uimm.eq(Cat(Repl(0, 12), instr[12:32])),
+            Iimm.eq(Cat(instr[20:31], Repl(instr[31], 21))),
+            Simm.eq(Cat(instr[7:12], Cat(instr[25:31], Repl(instr[31], 21)))),
+            Bimm.eq(Cat(0, Cat(instr[8:12], Cat(instr[25:31], Cat(
+                instr[7], Repl(instr[31], 20)))))),
+            Jimm.eq(Cat(0, Cat(instr[21:31], Cat(instr[20], Cat(
+                instr[12:20], Repl(instr[31], 12))))))
+        ]
+
+        # Register addresses decoder
+        m.d.comb += [
+            rs1Id.eq(instr[15:20]),
+            rs2Id.eq(instr[20:25]),
+            rdId.eq( instr[7:12])
+        ]
+
+        # Function code decdore
+        m.d.comb += [
+            funct3.eq(instr[12:15]),
+            funct7.eq(instr[25:32])
+        ]
+
+        # Fetch instruction and increase PC
+        if platform is None:
+            m.d.sync += [
+                instr.eq(mem[pc]),
+                pc.eq(Mux(isSystem, 0, pc + 1))
+            ]
+        else:
+            m.d.slow += [
+                    instr.eq(mem[pc]),
+                    pc.eq(Mux(isSystem, 0, pc + 1))
+            ]
+
+        # Assign important signals to LEDS
+        m.d.comb += self.leds.eq(Mux(isSystem, 31, Cat(isLoad, Cat(
+            isStore, Cat(isALUimm, Cat(isALUreg, pc[0]))))))
+
+        return m
