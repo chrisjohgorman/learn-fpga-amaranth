da8d5af62bb17d556963be69fa1d2913cca40b23
Chris Gorman <chrisjohgorman@gmail.com>
2024-10-30 09:07:37 -0400

    Add class and function descriptions
    - change a couple variable names

--- 05_register_bank/blink.py
+++ 05_register_bank/blink.py
@@ -10,19 +10,26 @@ from amaranth import Module, ClockDomain, Elaboratable
 
 class Top(Elaboratable):
 
+    """ This class sets pin assignments for the board leds.  It initializes the
+        clock domain to slow it down so we can see the leds blink on the board
+        describing the states of the finite state machine of the system on
+        chip. """
+
     def __init__(self, leds):
 
-        self.leds = leds
         self.soc = SOC()
+        self.leds = leds
 
     def elaborate(self, platform):
+
+        """ This function sets up the board.  A ClockDomain is initialized and
+            the leds of the board are connected to the leads of the SOC. """
+
         # We need a top level module
         m = Module()
 
-        soc = self.soc
-        leds = self.leds
-
         # This is the instance of our SOC
+        soc = self.soc
 
         # The SOC is turned into a submodule (fragment) of our top level
         # module.
@@ -31,17 +38,17 @@ class Top(Elaboratable):
         # for amaranth version 0.6 define slow domain in top module
         # Instantiate the clockwork with a divider of 2^21
         m.domains += ClockDomain(CLOCKWORKS_DOMAIN_NAME)
-        slclk = Clockworks(slow=21)
+        slow_clk = Clockworks(slow=21)
         # Turn the clockwork into a submodule of the top level module
-        m.submodules.slclk = slclk
+        m.submodules.slow_clk = slow_clk
 
         # We connect the SOC leds signal to the various LEDs on the board.
         m.d.comb += [
-            leds[0].o.eq(soc.leds[0]),
-            leds[1].o.eq(soc.leds[1]),
-            leds[2].o.eq(soc.leds[2]),
-            leds[3].o.eq(soc.leds[3]),
-            leds[4].o.eq(soc.leds[4]),
+            self.leds[0].o.eq(soc.leds[0]),
+            self.leds[1].o.eq(soc.leds[1]),
+            self.leds[2].o.eq(soc.leds[2]),
+            self.leds[3].o.eq(soc.leds[3]),
+            self.leds[4].o.eq(soc.leds[4]),
         ]
 
         return m
@@ -61,6 +68,6 @@ if __name__ in "__main__":
     led1 = platform.request('led', 1)
     rgb = platform.request('rgb_led')
 
-    leds = [led0, led1, rgb.r, rgb.g, rgb.b]
+    board_leds = [led0, led1, rgb.r, rgb.g, rgb.b]
 
-    platform.build(Top(leds), do_program=True)
+    platform.build(Top(board_leds), do_program=True)
