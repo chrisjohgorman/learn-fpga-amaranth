5ddc6d936ff24cc04cd33b6089a3b8b99ae4ad33
Chris Gorman <chrisjohgorman@gmail.com>
2024-10-31 15:42:05 -0400

    Quasi update to amaranth 0.5+ testbench

--- 04_instruction_decoder/bench.py
+++ 04_instruction_decoder/bench.py
@@ -3,7 +3,7 @@
 """ This module emulates the instruction decoder """
 
 from soc import SOC
-from amaranth.sim import Simulator, Period, Tick
+from amaranth.sim import Simulator, Period
 
 soc = SOC()
 
@@ -17,30 +17,39 @@ class Global:
     prev_pc = 0
 
 
-def proc():
+async def testbench(ctx):
 
     """ Proc function to test values from the instruction decoder. """
 
     while True:
-        pc = yield soc.pc
+        pc = ctx.get(soc.pc)
         if Global.prev_pc != pc:
             print(f"pc={pc}")
-            print(f"instr={(yield soc.instr):>032b}")
-            print(f"LEDS = {(yield soc.leds):>05b}")
-            if (yield soc.is_alu_reg):
-                print(f"ALUreg rd={(yield soc.rd_id)} rs1={(yield soc.rs1_id)}"
-                      f" rs2={(yield soc.rs2_id)} i_imm={(yield soc.i_imm)} "
-                      f"funct3={(yield soc.funct3)}")
-            if (yield soc.is_system):
+            print(f"instr={ctx.get(soc.instr):>032b}")
+            print(f"LEDS = {ctx.get(soc.leds):>05b}")
+            if ctx.get(soc.is_alu_reg):
+                print(f"ALUreg rd={ctx.get(soc.rd_id)} "
+                      f"rs1={ctx.get(soc.rs1_id)} "
+                      f"rs2={ctx.get(soc.rs2_id)} "
+                      f"i_imm={ctx.get(soc.i_imm)} "
+                      f"funct3={ctx.get(soc.funct3)}")
+            if ctx.get(soc.is_alu_imm):
+                print(f"ALUimm rd={ctx.get(soc.rd_id)} "
+                      f"rs1={ctx.get(soc.rs1_id)} "
+                      f"imm={ctx.get(soc.i_imm)} "
+                      f"funct3={ctx.get(soc.funct3)}")
+            if ctx.get(soc.is_load):
+                print("LOAD")
+            if ctx.get(soc.is_store):
+                print("STORE")
+            if ctx.get(soc.is_system):
                 print("SYSTEM")
-                break
-        yield Tick()
+        await ctx.tick()
         Global.prev_pc = pc
 
 
 sim.add_clock(Period(MHz=1))
-sim.add_process(proc)
+sim.add_testbench(testbench)
 
 with sim.write_vcd('bench.vcd', 'bench.gtkw', traces=soc.ports):
-    # Let's run for a quite long time
-    sim.run_until(Period(MHz=1) * 100)
+    sim.run_until(Period(MHz=1) * 10)
