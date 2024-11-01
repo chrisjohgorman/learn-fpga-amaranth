cf06b0e97a6eb2eceb67a988406d5af6eee23981
Chris Gorman <chrisjohgorman@gmail.com>
2024-11-01 10:51:35 -0400

    Convert bench.py to use amaranth 0.5+ for testbench and process functions

--- 05_register_bank/bench.py
+++ 05_register_bank/bench.py
@@ -3,16 +3,34 @@
 """ This module emulates the register bank """
 
 from soc import SOC
+from amaranth import Signal
 from amaranth.sim import Simulator, Period
 
-
+en = Signal(init=1)
+count = Signal(4)
 soc = SOC()
+
 
 class Global:
 
     """ Class with member to get rid of the use if the global keyword """
 
-    prev_clk = 0
+    prev_pc = 0
+
+
+async def process(ctx):
+
+    """ Process function to test values from the register bank.  
+        Create a counter to increment and wait for during the 
+        testbench function. """
+
+    count_value = 0  # initialize counter to 0
+    async for clk_edge, rst_value, en_value in ctx.tick().sample(en):
+        if rst_value:  # can be asserted with or without clk_edge
+            count_value = 0  # re-initialize counter
+        elif clk_edge and en_value:
+            count_value += 1  # advance the counter
+            ctx.set(count, count_value)  # publish its value to the simulation
 
 
 async def testbench(ctx):
@@ -20,35 +38,35 @@ async def testbench(ctx):
     """ Testbench function to test values from the register bank """
 
     while True:
-        clk = ctx.get(soc.cw.o_slow)
-        if Global.prev_clk == 0 and Global.prev_clk != clk:
-            print(f"pc={soc.pc}")
-            print(f"instr={ctx.get(soc.instr):>032b}")
-            print(f"LEDS = {ctx.get(soc.leds):>05b}")
-            if ctx.get(soc.is_alu_reg):
-                print(f"ALUreg rd={ctx.get(soc.rd_id)} "
-                      f"rs1={ctx.get(soc.rs1_id)} "
-                      f"rs2={ctx.get(soc.rs2_id)} "
-                      f"funct3={ctx.get(soc.funct3)}")
-            if ctx.get(soc.is_alu_imm):
-                print(f"ALUimm rd={ctx.get(soc.rd_id)} "
-                      f"rs1={ctx.get(soc.rs1_id)} "
-                      f"i_imm={ctx.get(soc.i_imm)} "
-                      f"funct3={ctx.get(soc.funct3)}")
-            if ctx.get(soc.is_load):
-                print("LOAD")
-            if ctx.get(soc.is_store):
-                print("STORE")
-            if ctx.get(soc.is_system):
-                print("SYSTEM")
-                break
-        await ctx.tick()
+        clk = ctx.get(count)
+        print(f"pc={ctx.get(soc.pc)}")
+        print(f"instr={ctx.get(soc.instr):>032b}")
+        print(f"LEDS = {ctx.get(soc.leds):>05b}")
+        if ctx.get(soc.is_alu_reg):
+            print(f"ALUreg rd={ctx.get(soc.rd_id)} "
+                  f"rs1={ctx.get(soc.rs1_id)} "
+                  f"rs2={ctx.get(soc.rs2_id)} "
+                  f"funct3={ctx.get(soc.funct3)}")
+        if ctx.get(soc.is_alu_imm):
+            print(f"ALUimm rd={ctx.get(soc.rd_id)} "
+                  f"rs1={ctx.get(soc.rs1_id)} "
+                  f"i_imm={ctx.get(soc.i_imm)} "
+                  f"funct3={ctx.get(soc.funct3)}")
+        if ctx.get(soc.is_load):
+            print("LOAD")
+        if ctx.get(soc.is_store):
+            print("STORE")
+        if ctx.get(soc.is_system):
+            print("SYSTEM")
+        # each instruction requires three clock ticks to execute
+        await ctx.tick().repeat(3)
         Global.prev_clk = clk
+
 
 sim = Simulator(soc)
 sim.add_clock(Period(MHz=1))
+sim.add_process(process)
 sim.add_testbench(testbench)
 
 with sim.write_vcd('bench.vcd', 'bench.gtkw', traces=soc.ports):
-    # Let's run for a quite long time
-    sim.run_until(Period(MHz=1) * 10000)
+    sim.run_until(Period(MHz=1) * 30)
