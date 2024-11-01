ab5b51628098cf570309272820a4c34cd11bbf22
Chris Gorman <chrisjohgorman@gmail.com>
2024-11-01 18:54:38 -0400

    06_alu: bench.py remove logic code for running pre amaranth 0.5

--- 06_alu/bench.py
+++ 06_alu/bench.py
@@ -4,7 +4,7 @@
 
 from soc import SOC
 from amaranth import Signal
-from amaranth.sim import Simulator, Period, Tick
+from amaranth.sim import Simulator, Period
 
 en = Signal(init=1)
 count = Signal(4)
@@ -13,9 +13,9 @@ soc = SOC()
 
 async def process(ctx):
 
-    """ Process function to test values from the register bank.
-        Create a counter to increment and wait for during the
-        testbench function. """
+    """ Process function to test values from the arithmetic logic unit.
+        Create a counter to increment and wait for during the testbench
+        function. """
 
     count_value = 0  # initialize counter to 0
     async for clk_edge, rst_value, en_value in ctx.tick().sample(en):
@@ -28,7 +28,7 @@ async def process(ctx):
 
 async def testbench(ctx):
 
-    """ Testbench function to test values from the register bank """
+    """ Testbench function to test values from the arithmetic logic unit """
 
     while True:
         state = ctx.get(soc.state)
@@ -63,46 +63,6 @@ async def testbench(ctx):
             print(f"  Execute: Writeback x{ctx.get(soc.rd_id)} "
                   f"= {ctx.get(soc.write_back_data):>032b}")
         await ctx.tick()
-# TODO remove block once debugging is complete.
-# class Global:
-#     prev_clk = 0
-#
-#
-# def proc():
-#     while True:
-#         print(f"{(yield soc.state)}")
-#         if Global.prev_clk == 0 and Global.prev_clk != (yield soc.cw.o_slow):
-#             state = (yield soc.state)
-#             if state == 2:
-#                 print("-- NEW CYCLE -----------------------")
-#                 print("  Fetch: LEDS = {:05b}".format((yield soc.leds)))
-#                 print("  Fetch: pc={}".format((yield soc.pc)))
-#                 print("  Fetch: instr={:#032b}".format((yield soc.instr)))
-#                 if (yield soc.is_alu_reg):
-#                     print("     ALUreg rd={} rs1={} rs2={} funct3={}".format(
-#                         (yield soc.rd_id), (yield soc.rs1_id), (yield soc.rs2_id),
-#                         (yield soc.funct3)))
-#                 if (yield soc.is_alu_imm):
-#                     print("     ALUimm rd={} rs1={} imm={} funct3={}".format(
-#                         (yield soc.rd_id), (yield soc.rs1_id), (yield soc.i_imm),
-#                         (yield soc.funct3)))
-#                 if (yield soc.is_load):
-#                     print("    LOAD")
-#                 if (yield soc.is_store):
-#                     print("    STORE")
-#                 if (yield soc.is_system):
-#                     print("    SYSTEM")
-#                     break
-#             if state == 4:
-#                 print("  Register: LEDS = {:05b}".format((yield soc.leds)))
-#                 print("  Register: rs1={}".format((yield soc.rs1)))
-#                 print("  Register: rs2={}".format((yield soc.rs2)))
-#             if state == 1:
-#                 print("  Execute: LEDS = {:05b}".format((yield soc.leds)))
-#                 print("  Execute: Writeback x{} = {:032b}".format((yield soc.rd_id),
-#                                                             (yield soc.write_back_data)))
-#         yield Tick()
-#         Global.prev_clk = yield soc.cw.o_slow
 
 
 sim = Simulator(soc)
@@ -111,4 +71,4 @@ sim.add_process(process)
 sim.add_testbench(testbench)
 
 with sim.write_vcd('bench.vcd', 'bench.gtkw', traces=soc.ports):
-    sim.run_until(Period(MHz=1) * 60)
+    sim.run_until(Period(MHz=1) * 45)
