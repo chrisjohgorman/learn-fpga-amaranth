5b553f57eff8551a2d018a223d28a3932cb1ab69
Chris Gorman <chrisjohgorman@gmail.com>
2024-11-10 14:46:45 -0500

    12_size_optimization initial import
    - debugging information.  this step produces a different
      write_back_data than the previous step.  This could mean
      that a bug has been introduced at this step.

---
+++ 12_size_optimisation/memory.py
@@ -0,0 +1,41 @@
+from riscv_assembler import RiscvAssembler
+from amaranth import Elaboratable, Module, Array, Signal
+
+
+class Memory(Elaboratable):
+
+    def __init__(self):
+        a = RiscvAssembler()
+
+        a.read("""begin:
+        ADD x1, x0, x0
+        ADDI x2, x0, 31
+        l0:
+        ADDI x1, x1, 1
+        BNE x1, x2, l0
+        EBREAK
+        """)
+
+        a.assemble()
+        self.instructions = a.mem
+        print(f"memory = {self.instructions}")
+
+        # Instruction memory initialised with above instructions
+        self.mem = Array([Signal(32, init=x, name="mem")
+                          for x in self.instructions])
+
+        self.mem_addr = Signal(32)
+        self.mem_rdata = Signal(32)
+        self.mem_rstrb = Signal()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        if platform is None:
+            with m.If(self.mem_rstrb):
+                m.d.sync += self.mem_rdata.eq(self.mem[self.mem_addr[2:32]])
+        else:
+            with m.If(self.mem_rstrb):
+                m.d.slow += self.mem_rdata.eq(self.mem[self.mem_addr[2:32]])
+
+        return m
