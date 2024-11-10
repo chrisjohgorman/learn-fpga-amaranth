24a06e2e8dcf87edcbb8d1274f08d71b7049d32d
Chris Gorman <chrisjohgorman@gmail.com>
2024-11-10 12:18:27 -0500

    11_modules, bench.py changes for amaranth 0.6
    - specify import of all modules specifically without use of *
    - add process with count and signal to iterate over testbench
    - replace use of yield with ctx.get
    - replace .format use in print with f-strings

--- 11_modules/bench.py
+++ 11_modules/bench.py
@@ -1,61 +1,76 @@
-from amaranth import *
-from amaranth.sim import *
+#!/usr/bin/env python3
+
+""" This module uses the riscv-assembly module to improve the readability
+    of the code used in the arithmetic logic unit (ALU). """
 
 from soc import SOC
+from amaranth import Signal
+from amaranth.sim import Simulator, Period
 
+en = Signal(init=1)
+count = Signal(4)
 soc = SOC()
 
-sim = Simulator(soc)
 
-prev_clk = 0
+async def process(ctx):
 
-def proc():
+    """ Process function to test values from the arithmetic logic unit
+        using the riscv-assembler.  Create a counter to increment and
+        wait for during the testbench function. """
+
+    count_value = 0  # initialize counter to 0
+    async for clk_edge, rst_value, en_value in ctx.tick().sample(en):
+        if rst_value:  # can be asserted with or without clk_edge
+            count_value = 0  # re-initialize counter
+        elif clk_edge and en_value:
+            count_value += 1  # advance the counter
+            ctx.set(count, count_value)  # publish its value to the simulation
+
+
+async def testbench(ctx):
     cpu = soc.cpu
-    mem = soc.memory
     while True:
-        global prev_clk
-        clk = yield soc.slow_clk
-        if prev_clk == 0 and prev_clk != clk:
-            state = (yield soc.cpu.fsm.state)
-            if state == 2:
-                print("-- NEW CYCLE -----------------------")
-                print("  F: LEDS = {:05b}".format((yield soc.leds)))
-                print("  F: pc={}".format((yield cpu.pc)))
-                print("  F: instr={:#032b}".format((yield cpu.instr)))
-                if (yield cpu.isALUreg):
-                    print("     ALUreg rd={} rs1={} rs2={} funct3={}".format(
-                        (yield cpu.rdId), (yield cpu.rs1Id), (yield cpu.rs2Id),
-                        (yield cpu.funct3)))
-                if (yield cpu.isALUimm):
-                    print("     ALUimm rd={} rs1={} imm={} funct3={}".format(
-                        (yield cpu.rdId), (yield cpu.rs1Id), (yield cpu.Iimm),
-                        (yield cpu.funct3)))
-                if (yield cpu.isBranch):
-                    print("    BRANCH rs1={} rs2={}".format(
-                        (yield cpu.rs1Id), (yield cpu.rs2Id)))
-                if (yield cpu.isLoad):
-                    print("    LOAD")
-                if (yield cpu.isStore):
-                    print("    STORE")
-                if (yield cpu.isSystem):
-                    print("    SYSTEM")
-                    break
-            if state == 4:
-                print("  R: LEDS = {:05b}".format((yield soc.leds)))
-                print("  R: rs1={}".format((yield cpu.rs1)))
-                print("  R: rs2={}".format((yield cpu.rs2)))
-            if state == 1:
-                print("  E: LEDS = {:05b}".format((yield soc.leds)))
-                print("  E: Writeback x{} = {:032b}".format((yield cpu.rdId),
-                                             (yield cpu.writeBackData)))
-            if state == 8:
-                print("  NEW")
-        yield
-        prev_clk = clk
+        state = ctx.get(soc.cpu.fsm.state)
+        if state == 2:
+            print("-- NEW CYCLE -----------------------")
+            print(f"  F: LEDS = {ctx.get(soc.leds):>05b}")
+            print(f"  F: pc={ctx.get(cpu.pc)}")
+            print(f"  F: instr={ctx.get(cpu.instr):>032b}")
+            if ctx.get(cpu.is_alu_reg):
+                print(f"     alu_reg rd={ctx.get(cpu.rd_id)} "
+                      f"rs1={ctx.get(cpu.rs1_id)} "
+                      f"rs2={ctx.get(cpu.rs2_id)} "
+                      f"funct3={ctx.get(cpu.funct3)}")
+            if ctx.get(cpu.is_alu_imm):
+                print(f"     alu_imm rd={ctx.get(cpu.rd_id)} "
+                      f"rs1={ctx.get(cpu.rs1_id)} "
+                      f"imm={ctx.get(cpu.i_imm)} "
+                      f"funct3={ctx.get(cpu.funct3)}")
+            if ctx.get(cpu.is_branch):
+                print(f"    BRANCH rs1={ctx.get(cpu.rs1_id)} "
+                      f"rs2={ctx.get(cpu.rs2_id)}")
+            if ctx.get(cpu.is_load):
+                print("    LOAD")
+            if ctx.get(cpu.is_store):
+                print("    STORE")
+            if ctx.get(cpu.is_system):
+                print("    SYSTEM")
+        if state == 4:
+            print(f"  R: LEDS = {ctx.get(soc.leds):>05b}")
+            print(f"  R: rs1={ctx.get(cpu.rs1)}")
+            print(f"  R: rs2={ctx.get(cpu.rs2)}")
+        if state == 1:
+            print(f"  E: LEDS = {ctx.get(soc.leds):>05b}")
+            print(f"  E: Writeback x{ctx.get(cpu.rd_id)} = "
+                  f"{ctx.get(cpu.write_back_data):>032b}")
+        if state == 8:
+            print("  NEW")
+        await ctx.tick()
 
-sim.add_clock(1e-6)
-sim.add_sync_process(proc)
+sim = Simulator(soc)
+sim.add_clock(Period(MHz=1))
+sim.add_process(process)
+sim.add_testbench(testbench)
 
 with sim.write_vcd('bench.vcd', 'bench.gtkw', traces=soc.ports):
-    # Let's run for a quite long time
-    sim.run_until(2, )
+    sim.run_until(Period(MHz=1) * 280)
