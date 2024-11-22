61797bf2432c075b262505353c1dcf13bca1fd1f
Chris Gorman <chrisjohgorman@gmail.com>
2024-11-22 10:29:15 -0500

    17_memory_map, bench.py, blink.py, cpu.py, memory.py, soc.py
    - initial import NOT functional at this moment
    - also import uart_tx.py

---
+++ 17_memory_map/blink.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+""" This module blinks the leds on a Cmod A7 FPGA slowly enough to see """
+
+from soc import SOC
+from clockworks import Clockworks, CLOCKWORKS_DOMAIN_NAME
+# from amaranth_boards.cmod_a7 import CmodA7_35Platform, Resource, Subsignal, \
+#         Pins, Attrs
+# from amaranth import Module, ClockDomain, Elaboratable
+from amaranth_boards.cmod_a7 import *
+from amaranth import * 
+from amaranth.build import *
+
+
+class Top(Elaboratable):
+
+    """ This class sets pin assignments for the board leds.  It initializes the
+        clock domain to slow it down so we can see the leds blink on the board
+        describing the states of the finite state machine of the system on
+        chip. """
+
+    def __init__(self, leds, uart):
+
+        self.soc = SOC()
+        self.leds = leds
+        self.uart = uart
+
+    def elaborate(self, platform):
+
+        """ This function sets up the board.  A ClockDomain is initialized and
+            the leds of the board are connected to the leads of the SOC. """
+
+        # We need a top level module
+        m = Module()
+
+        # This is the instance of our SOC
+        soc = self.soc
+        leds = self.leds
+        uart = self.uart
+
+        # The SOC is turned into a submodule (fragment) of our top level
+        # module.
+        m.submodules.soc = soc
+
+        # for amaranth version 0.6 define slow domain in top module
+        m.domains += ClockDomain(CLOCKWORKS_DOMAIN_NAME)
+        slow_clk = Clockworks()
+        # Turn the clockwork into a submodule of the top level module
+        m.submodules.slow_clk = slow_clk
+
+        # We connect the SOC leds signal to the various LEDs on the board.
+        m.d.comb += [
+            leds[0].o.eq(soc.leds[0]),
+            leds[1].o.eq(soc.leds[1]),
+            leds[2].o.eq(soc.leds[2]),
+            leds[3].o.eq(soc.leds[3]),
+            leds[4].o.eq(soc.leds[4]),
+        ]
+
+        # Connect the tx port to the SOC
+        if hasattr(soc, "tx"):
+            m.d.comb += [
+                uart.tx.eq(soc.tx)
+            ]
+
+        return m
+
+
+if __name__ in "__main__":
+
+    # A platform contains board specific information about FPGA pin
+    # assignments, toolchain and specific information for uploading the
+    # bitfile.
+    platform = CmodA7_35Platform(toolchain="Vivado")
+    gpio = ("gpio", 0)
+    platform.add_resources([
+        Resource("uart", 1,
+                 Subsignal("tx", Pins("1", conn=gpio, dir='o')),
+                 Subsignal("rx", Pins("2", conn=gpio, dir='i')),
+                 Attrs(IOSTANDARD="LVCMOS33"))
+    ])
+    # To generate the bitstream, we build() the platform using our top level
+    # module m.
+    # The platform allows access to the various resources defined by the board
+    # definition from amaranth-boards.
+    led0 = platform.request('led', 0)
+    led1 = platform.request('led', 1)
+    rgb = platform.request('rgb_led')
+
+    board_leds = [led0, led1, rgb.r, rgb.g, rgb.b]
+    uart = platform.request('uart', 1)
+
+    platform.build(Top(board_leds, uart), do_program=True)
---
+++ 17_memory_map/soc.py
@@ -0,0 +1,105 @@
+""" This is the system on chip module. """
+
+from memory import Mem
+from cpu import CPU
+from uart_tx import UartTx
+from amaranth import Elaboratable, Signal, Module, DomainRenamer, Cat, C, Mux
+
+
+class SOC(Elaboratable):
+
+    """ This class describes the SOC. """
+
+    def __init__(self):
+
+        self.leds = Signal(5)
+        self.tx = Signal()
+
+        self.cpu = None
+        self.memory = None
+
+    def elaborate(self, platform):
+
+        """ Here we connect the CPU and peripherals.  The memory module
+            mem_addr, mem_rstrb and mem_rdata get connected to their
+            respective CPU counterparts.  Then we connect the x10 register
+            to the LEDs.  And finally we connect the UART module. """
+
+        m = Module()
+
+        if platform is None:
+            memory = Mem()
+            cpu = CPU()
+        else:
+            clk_frequency = int(platform.default_clk_constraint.frequency)
+            print(f"clock frequency = {clk_frequency}")
+            memory = DomainRenamer("slow")(Mem())
+            cpu = DomainRenamer("slow")(CPU())
+            uart_tx = DomainRenamer("slow")(
+                    UartTx(freq_hz=clk_frequency, baud_rate=1000000))
+
+        m.submodules.cpu = cpu
+        m.submodules.memory = memory
+        if platform is not None:
+            m.submodules.uart_tx = uart_tx
+
+        self.cpu = cpu
+        self.memory = memory
+
+        ram_rdata = Signal(32)
+        mem_wordaddr = Signal(30)
+        is_io = Signal()
+        is_ram = Signal()
+        mem_wstrb = Signal()
+        io_rdata = Signal(32)
+
+        # Memory map bits
+        io_leds_bit = 0
+        io_uart_dat_bit = 1
+        io_uart_cntl_bit = 2
+
+        m.d.comb += [
+            mem_wordaddr.eq(cpu.mem_addr[2:32]),
+            is_io.eq(cpu.mem_addr[22]),
+            is_ram.eq(~is_io),
+            mem_wstrb.eq(cpu.mem_wmask.any())
+        ]
+
+        # Connect memory to CPU
+        m.d.comb += [
+            memory.mem_addr.eq(cpu.mem_addr),
+            memory.mem_rstrb.eq(is_ram & cpu.mem_rstrb),
+            memory.mem_wdata.eq(cpu.mem_wdata),
+            memory.mem_wmask.eq(is_ram.replicate(4) & cpu.mem_wmask),
+            ram_rdata.eq(memory.mem_rdata),
+            cpu.mem_rdata.eq(Mux(is_ram, ram_rdata, io_rdata))
+        ]
+
+        # LEDs
+        with m.If(is_io & mem_wstrb & mem_wordaddr[io_leds_bit]):
+            m.d.sync += self.leds.eq(cpu.mem_wdata)
+
+        if platform is not None:
+            # UART
+            uart_valid = Signal()
+            uart_ready = Signal()
+
+            m.d.comb += [
+                uart_valid.eq(is_io & mem_wstrb & mem_wordaddr[io_uart_dat_bit])
+            ]
+
+            # Hook up UART
+            m.d.comb += [
+                uart_tx.valid.eq(uart_valid),
+                uart_tx.data.eq(cpu.mem_wdata[0:8]),
+                uart_ready.eq(uart_tx.ready),
+                self.tx.eq(uart_tx.tx)
+            ]
+
+            # Data from UART
+            m.d.comb += [
+                io_rdata.eq(Mux(mem_wordaddr[io_uart_cntl_bit],
+                                Cat(C(0, 9), ~uart_ready, C(0, 22)), C(0, 32)))
+            ]
+
+        return m
